6c544470f3dcc99f6ff62d88350be61958b116ff
HandyGuySoftware <steve@friedfamily.net>
2020-02-23 07:30:38 -0600

    Release 2.2.11

--- globs.py
+++ globs.py
@@ -9,7 +9,7 @@ import os
 
 # Define version info
 version=[2,2,11]     # Program Version
-status='Beta 1'
+status='Release'
 dbVersion=[1,0,3]   # Required DB version
 rcVersion=[3,0,0]   # Required RC version
 copyright='Copyright (c) 2020 Stephen Fried for HandyGuy Software.'
