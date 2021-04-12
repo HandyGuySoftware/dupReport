2f944ef23cae5f62d7a6b91b69b8a9c3974be7b4
HandyGuySoftware <steve@friedfamily.net>
2021-04-12 15:09:31 -0500

    Issue #166

--- globs.py
+++ globs.py
@@ -9,7 +9,7 @@ import os
 
 # Define version info
 version=[3,0,5]     # Program Version
-status='Beta - Issue #166'
+status='Beta 2 - Issue #166'
 dbVersion=[3,0,1]   # Required DB version
 rcVersion=[3,1,0]   # Required RC version
 copyright='Copyright (c) 2017-2021 Stephen Fried for Handy Guy Software.'
